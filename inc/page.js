91e82db63a22568ddbe2f01354179d6f407a4c9f
Walter A. Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2019-10-17 18:36:23 +0200

    Added local libraries, minor fix

--- inc/page.js
+++ inc/page.js
@@ -1,8 +1,11 @@
 (function($) {
+
   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  // On menue click, smooth scrolling using jQuery easing
+
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"]):not(.normal-link)').click( function() {
+    
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -13,14 +16,17 @@
         return false;
       }
     }
+
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+
+  $('.js-scroll-trigger').click( function() {
     $('.navbar-collapse').collapse('hide');
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
+
   $('body').scrollspy({
     target: '#mainNav',
     offset: 56
